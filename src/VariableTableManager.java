d9644cda385f964d1aef77ca8eb59a73f39bffb8
s4e-tom <tom.solacroup@gmail.com>
2013-12-17 23:20:10 +0100

    version finale

--- src/VariableTableManager.java
+++ src/VariableTableManager.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
 public class VariableTableManager{
-	public static void verify(HashMap hm, String name, EnumType type){
+	public static void verify(HashMap hm, String name){
 		//verifier les deux cas suivants
-		if(hm.containsKey(name)){//variable non trouvée
-			System.out.println("Error: variable "+name+" not declared!");
+		if(!hm.containsKey(name)){//variable non trouvée
+			System.out.println("---------------------\n Error: variable "+name+" not declared! \n---------------------");
 		}
 		/*if(hm.get(name) != type){ //variable trouvée mais de type différent
 			System.err.println("Warning: incompatible types! ("+hm.get(name)+" and "+type);
