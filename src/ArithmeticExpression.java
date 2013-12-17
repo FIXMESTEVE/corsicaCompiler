097c3ee6a34fc53da8d9eb79598fca29b8b0b438
s4e-dev <tom.solacroup@gmail.com>
2013-12-17 21:24:19 +0100

    ArithmeticExpression hotfix

--- src/ArithmeticExpression.java
+++ src/ArithmeticExpression.java
@@ -2,6 +2,6 @@ public abstract class ArithmeticExpression implements Expression{
 	public void generateJump(Label yes, Label no){
 		Address res = generateCode();
 		System.out.println("if " + res.print() + " != 0 goto " + yes.print());
-		System.out.println("goto" + no.print());
+		System.out.println("goto " + no.print());
 	}
 }
